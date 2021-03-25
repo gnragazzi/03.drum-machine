0d16072b96567f340a3f442c0dc9a865d231f366
gnragazzi <gnragazzi@gmail.com>
2021-03-25 11:39:55 -0300

    Uploaded Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect, useState, useRef } from 'react'
+import data from './data'
 
 function App() {
+  const [innerText, setInnerText] = useState('')
+  const displayRef = useRef(null)
+  // const [key]
+  const handleKeyDown = (e) => {
+    const key = e.key.toUpperCase()
+    const audioDom = document.getElementById(key)
+    if (audioDom) {
+      const text = audioDom.dataset.label
+      setInnerText(text)
+      audioDom.play()
+    }
+  }
+
+  const handleClick = (e) => {
+    e.currentTarget.children[0].play()
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main id='drum-machine' onKeyPress={() => console.log('pressed key')}>
+      <article>
+        {data.map(({ name, url, label }, index) => {
+          return (
+            <button
+              key={index}
+              className='drum-pad'
+              id={label}
+              onClick={handleClick}
+            >
+              <audio
+                id={name}
+                className='clip'
+                src={url}
+                data-label={`${label}`}
+              ></audio>
+              {name}
+            </button>
+          )
+        })}
+        <h1 id='display' ref={displayRef}>
+          {innerText}
+        </h1>
+      </article>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    name: 'Q',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/21[kb]new_closed_hat%202.wav.mp3',
+    label: 'Close Hat',
+  },
+  {
+    name: 'W',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/80[kb]wideopenhh.wav.mp3',
+    label: 'Open-Hat',
+  },
+  {
+    name: 'E',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Crashes/102[kb]crappy-crash.wav.mp3',
+    label: 'Crash',
+  },
+  {
+    name: 'A',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/44[kb]lotom.wav.mp3',
+    label: 'Low-Tom',
+  },
+  {
+    name: 'S',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/42[kb]midtom.wav.mp3',
+    label: 'Mid-Tom',
+  },
+  {
+    name: 'D',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/39[kb]hitom.wav.mp3',
+    label: 'High-Tom',
+  },
+  {
+    name: 'Z',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/21[kb]distorted-filter-kick-small.wav.mp3',
+    label: 'Kick',
+  },
+  {
+    name: 'X',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Snares/18[kb]solidsnare.wav.mp3',
+    label: 'Snare',
+  },
+  {
+    name: 'C',
+    url:
+      'https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Snares/44[kb]solid_rimshot.wav.mp3',
+    label: 'Rim Shot',
+  },
+]
